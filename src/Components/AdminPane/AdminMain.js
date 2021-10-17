e5fd940d74e140322258d25a80f10836ceaff78e
safidark <safi29317@gmail.com>
2021-10-17 20:11:21 +0600

    shafi

--- src/Components/AdminPane/AdminMain.js
+++ src/Components/AdminPane/AdminMain.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import SidebarMain from '../Shared/Navbar/Sidebar/SidebarMain';
 import Guitar from './Guitar/Guitar';
+import Navbar from './../Shared/Navbar/Navbar';
 const AdminMain = () => {
     return (
         <div className="bg-dark row pb-4" style={{height:''}}>
+        <Navbar/>
             <div className="col-md-3">
                 <SidebarMain/>
             </div>
