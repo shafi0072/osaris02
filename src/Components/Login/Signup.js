7459604441028d9d322ec050742b6d9e36af90b0
safidark <safi29317@gmail.com>
2021-11-08 10:31:16 +0600

    shafi

--- src/Components/Login/Signup.js
+++ src/Components/Login/Signup.js
@@ -1,39 +1,103 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Login.css';
+import {getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+import './firebase.config';
+
+
+
 
 const Signup = () => {
+    const [user, SetUser] = useState({});
+    const handleOnChange = (e) => {
+        const newUser = {
+            ...user
+        };
+        newUser[e.target.name] = e.target.value;
+        SetUser(newUser);
+        console.log(user);
+    }
+    const handleOnSubmit = (e) => {
+        const auth = getAuth();
+        createUserWithEmailAndPassword(auth, user.email, user.password)
+            .then(
+                (userCredential) => {
+                    // Signed in
+                    const user = userCredential.user;
+                    console.log("work successfully")
+                    SetUser({success:true});
+                }
+            )
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+            });
+        e.preventDefault()
+    }
     return (
         <div className="main-login">
             <h1 className="signup-logo">OSARIS</h1>
             <h2 className="login-title">Create an account</h2>
-            <div  className="main-login-container">
+            <div className="main-login-container">
                 <div className="login-container">
-                <form>
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">First Name</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Last Name</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputEmail1" class="form-label">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1"/>
-                    </div>
-                    <div class="mb-3">
-                        <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1"/>
-                    </div>
-                    <button type="submit" class="btn btn-primary login-btn">Create Profile</button>
-                    <p className="mt-3 text-center">Have you an accout?<span className="signup-link"> Log in</span></p>
+                    <form onSubmit={handleOnSubmit}>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">First Name</label>
+                            <input
+                                type="text"
+                                class="form-control"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                                name="fN"
+                                onChange={handleOnChange}/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Last Name</label>
+                            <input
+                                type="text"
+                                class="form-control"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                                name="LN"
+                                onChange={handleOnChange}/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input
+                                type="email"
+                                class="form-control"
+                                id="exampleInputEmail1"
+                                aria-describedby="emailHelp"
+                                name="email"
+                                onChange={handleOnChange}/>
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input
+                                type="password"
+                                class="form-control"
+                                id="exampleInputPassword1"
+                                name="password"
+                                onChange={handleOnChange}/>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+                            <input
+                                type="password"
+                                class="form-control"
+                                id="exampleInputPassword1"
+                                name="confirmPassword"
+                                onChange={handleOnChange}/>
+                        </div>
+                        {user.password === user.confirmPassword &&<button type="submit" class="btn btn-primary login-btn">Create Profile</button>}
+                     <p className="mt-3 text-center">Have you an accout?<span className="signup-link">
+                        Log in</span>
+                    </p>
+                    {user.password !== user.confirmPassword && <p className="text-danger">Sorry buddy the password isn't match</p>}
+                    {user.success && <p className="text-success">Congratulations Buddy your account created successfully</p>}
                     </form>
-            </div>
+                </div>
             </div>
         </div>
     );
---
+++ src/Components/Login/firebase.config.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from 'firebase/app';
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyA6XQ5x9_zYMLYNJ2RjTqh9bU1zHtu9xkM",
+    authDomain: "guitarcare-324ce.firebaseapp.com",
+    projectId: "guitarcare-324ce",
+    storageBucket: "guitarcare-324ce.appspot.com",
+    messagingSenderId: "681384678216",
+    appId: "1:681384678216:web:ce179d4fdfeeb17244597d",
+    measurementId: "G-0V2MVMNMP3"
+  };
+const app = initializeApp(firebaseConfig);
+export default app;
