e40862ce73f66cd8743065d458cd1fe26310892b
safidark <safi29317@gmail.com>
2021-12-08 23:58:32 +0600

    shafi

--- src/Components/ClientBuilder/ClientBuilder.js
+++ src/Components/ClientBuilder/ClientBuilder.js
@@ -4,6 +4,8 @@ import Banner from "./Banner";
 import BuildCategory from './BuildCategory.js';
 import BuilderInput from './BuilderInput';
 import Footer from './../Home/Footer/Footer';
+import WoodCollection from './WoodCollection';
+
 
 const ClientBuilder = () => {
     return (
@@ -11,6 +13,7 @@ const ClientBuilder = () => {
             <Navbar/>
             <Banner/>
             <BuildCategory/>
+            <WoodCollection/>
             <BuilderInput/>
             <Footer/>
         </div>
---
+++ src/Components/ClientBuilder/WoodCollection.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import './ClientBuilder';
+import img1 from '../../Resorce/Wood/Screenshot 2021-12-08 200644.png'
+import img2 from '../../Resorce/Wood/Screenshot 2021-12-08 200724.png';
+import img3 from '../../Resorce/Wood/Screenshot 2021-12-08 200739.png';
+import img4 from '../../Resorce/Wood/Screenshot 2021-12-08 200820.png';
+import img5 from '../../Resorce/Wood/Screenshot 2021-12-08 200842.png';
+import img6 from '../../Resorce/Wood/Screenshot 2021-12-08 200859.png';
+import img7 from '../../Resorce/Wood/Screenshot 2021-12-08 200917.png';
+import img8 from '../../Resorce/Wood/Screenshot 2021-12-08 200935.png';
+import img9 from '../../Resorce/Wood/Screenshot 2021-12-08 200955.png';
+import img10 from '../../Resorce/Wood/Screenshot 2021-12-08 201009.png';
+import img11 from '../../Resorce/Wood/Screenshot 2021-12-08 201023.png';
+import img12 from '../../Resorce/Wood/Screenshot 2021-12-08 201037.png';
+import img13 from '../../Resorce/Wood/Screenshot 2021-12-08 201112.png'
+import img14 from '../../Resorce/Wood/Screenshot 2021-12-08 201125.png';
+import img15 from '../../Resorce/Wood/Screenshot 2021-12-08 201142.png';
+import img16 from '../../Resorce/Wood/Screenshot 2021-12-08 201154.png';
+const WoodCollection = () => {
+    return (
+        <div className='woodCollection pt-5'>
+            <h1 className="text-light h1 text-center">WOOD COLLECTION</h1>\
+            <p className="text-light  text-center">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur <br />
+There is no one who loves pain itself, who seeks after it and wants to have it, simply</p>
+            <div className="row  ">
+                <div className="col-md-2 d-flex justify-content-center">
+                    <div>
+                        <img src={img1} alt="" />
+                    </div>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center">
+                <div>
+                        <img src={img2} alt="" style={{}}/>
+                    </div>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center">
+                <div>
+                        <img src={img3} alt="" style={{}}/>
+                    </div>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center">
+                <div>
+                        <img src={img4} alt="" style={{}}/>
+                    </div>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center">
+                <div>
+                        <img src={img5} alt="" style={{}}/>
+                    </div>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center">
+                <div>
+                        <img src={img6} alt="" style={{}}/>
+                    </div>
+                </div>
+            </div>
+            <div className="row mt-5">
+                <div className="col-md-2 d-flex justify-content-center">
+                    <div>
+                        <img src={img7} alt="" style={{}}/>
+                    </div>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center">
+                <div>
+                        <img src={img8} alt="" style={{}}/>
+                    </div>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center">
+                <div>
+                        <img src={img9} alt="" style={{}}/>
+                    </div>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center">
+                <div>
+                        <img src={img10} alt="" style={{}}/>
+                    </div>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center">
+                <div>
+                        <img src={img11} alt="" style={{}}/>
+                    </div>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center">
+                <div>
+                        <img src={img12} alt="" style={{}}/>
+                    </div>
+                </div>
+            </div>
+            <div className="row mt-5">
+                <div className="col-md-2 d-flex justify-content-center">
+                    <div>
+                        
+                    </div>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center">
+                <div>
+                        <img src={img13} alt="" style={{}}/>
+                    </div>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center">
+                <div>
+                        <img src={img14} alt="" style={{}}/>
+                    </div>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center">
+                <div>
+                        <img src={img15} alt="" style={{}}/>
+                    </div>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center">
+                <div>
+                        <img src={img16} alt="" style={{}}/>
+                    </div>
+                </div>
+                <div className="col-md-2 d-flex justify-content-center">
+                <div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default WoodCollection;
