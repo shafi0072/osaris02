67db23c310580daee19f1b22f47f4d84292bd0dd
safidark <safi29317@gmail.com>
2021-12-08 19:43:35 +0600

    shafi

--- src/Components/ClientBuilder/BuildCategory.js
+++ src/Components/ClientBuilder/BuildCategory.js
@@ -9,18 +9,18 @@ const BuildCategory = () => {
             </div>
            </div>
            <p className='text-center text-light mt-2'>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet</p>
-            <div className=" d-flex justify-content-evenly mt-5">
-                <div>
+            <div className=" row  mt-5" style={{paddingLeft:'10%'}}>
+                <div className="col-md-4">
                     <div className='categoryDv'>
                         <h1 className='text-light'>Acousitc</h1>
                     </div>
                 </div>
-                <div>
+                <div className="col-md-4">
                     <div className='categoryDv'>
                         <h1 className='text-light'>Electric</h1>
                     </div>
                 </div>
-                <div>
+                <div className="col-md-4">
                     <div className='categoryDv'>
                         <h1 className='text-light'>Bass</h1>
                     </div>
