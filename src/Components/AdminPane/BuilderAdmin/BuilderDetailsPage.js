93a00ee0caff342db277e6253ac25c15fe40cda6
safidark <safi29317@gmail.com>
2021-12-12 22:36:05 +0600

    shafi

--- src/Components/AdminPane/BuilderAdmin/BuilderDetailsPage.js
+++ src/Components/AdminPane/BuilderAdmin/BuilderDetailsPage.js
@@ -10,13 +10,13 @@ const BuilderDetailsPage = () => {
     useEffect(() => {
         fetch(`http://localhost:5000/builder/${id}`)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => setSingleData(data))
         .catch(err => console.log(err))
     }, [id])
     
     return (
         <div>
-            <h1>Hello its page admin Details</h1>
+            <h1>Details about <span>{singleData[0].fullName}</span> Order</h1>
         </div>
     );
 };
