a64e6371961922bac78b5e3580d55bd16e5f8bc2
Raktim02 <you@example.com>
2021-12-14 20:36:14 +0600

    raktim

--- src/Components/Home/MadeLocation/MadeLocation.js
+++ src/Components/Home/MadeLocation/MadeLocation.js
@@ -7,21 +7,21 @@ const MadeLocation = () => {
         <div className='bangladesh-container'>
             <img className='background' src={BackgroundImg}/>
             <div className='row bangladesh-text-container'>
-                <div className='col-md-7'>
+                <div className='col-md-7 bangladesh-text'>
                     <div>
-                        <h1>PROUDLY MADE IN <br/>BANGLADESH</h1>
-                        <p>Over the past 5 years, we’ve worked dedicated & passionate to build something that brings happiness to the Musicians around the country.
+                        <h1 className='bangladesh-h1'>PROUDLY MADE IN <br/>BANGLADESH</h1>
+                        <p className='bangladesh-p'>Over the past 5 years, we’ve worked dedicated & passionate to build something that brings happiness to the Musicians around the country.
                         Our very own built instruments, which are both affordable & keeps up with
                         the quality. The greatest gratification is to receive recognition for what
                         we built. This motivates us to improve even better. Let’s build a better
                         future for the musicians and local manufacturers to grow together and
                         proudly introduce our country on the global platform.</p>
-                        <h4>We Are Proudly Made in Bangladesh</h4>
+                        <h4 className='bangladesh-h4'>We Are Proudly Made in Bangladesh</h4>
                     </div>
                 </div>
                 <div className='col-md-5'>
                     <div>
-                        <img src={BangladeshMap}/>
+                        <img className='bangladesh-map' src={BangladeshMap}/>
                     </div>
                 </div>
             </div>
